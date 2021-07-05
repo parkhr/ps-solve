74e2e2ed3818781e24fdffecbce82afb1075e34a
parkhr <46472772+parkhr@users.noreply.github.com>
2021-07-05 23:17:21 +0900

    Update 20.java

--- leetcode/20.java
+++ leetcode/20.java
@@ -1,12 +1,14 @@
-public boolean isValid(String s) {
-    Stack<Character> stack = new Stack<>();
-    for (int i = 0; i < s.length(); i++) {
-        if(stack.isEmpty()) stack.push(s.charAt(i));
-        else if (s.charAt(i) == '}' && stack.peek() == '{') stack.pop();
-        else if(s.charAt(i) == ')' && stack.peek() == '(') stack.pop();
-        else if(s.charAt(i) == ']' && stack.peek() == '[') stack.pop();
-        else stack.push(s.charAt(i));
-    }
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(stack.isEmpty()) stack.push(s.charAt(i));
+            else if (s.charAt(i) == '}' && stack.peek() == '{') stack.pop();
+            else if(s.charAt(i) == ')' && stack.peek() == '(') stack.pop();
+            else if(s.charAt(i) == ']' && stack.peek() == '[') stack.pop();
+            else stack.push(s.charAt(i));
+        }
 
-    return stack.isEmpty() ? true : false;
+        return stack.isEmpty() ? true : false;
+    }
 }
