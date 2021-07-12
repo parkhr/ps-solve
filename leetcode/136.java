9d5fa6e547617502e01486b14fddfbb45c80e942
parkhr <46472772+parkhr@users.noreply.github.com>
2021-07-12 21:52:36 +0900

    Create 136.java

---
+++ leetcode/136.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(list.contains(nums[i])){
+                list.remove(new Integer(nums[i]));
+            }else{
+                list.add(nums[i]);
+            }
+        }
+
+        return list.get(0);
+    }
+}
